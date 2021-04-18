23b5ae18ffe76d1c11cafe848d1ac90ef50d487b
HarionPlayZ <debugkeklov@yandex.ru>
2021-04-18 21:21:17 +0300

    Update custody_pd2_icon.lua

--- gamemodes/payday/gamemode/client/custody_pd2_icon.lua
+++ gamemodes/payday/gamemode/client/custody_pd2_icon.lua
@@ -1,6 +1,8 @@
 local ourMat = Material("custody.png")
 local ourMat2 = Material("pr1.png")
 local ourMat3 = nil
+local dif1 = {'Normal', 'Hard', 'Very Hard', 'Overkill', 'Mayhem', 'Death Wish', 'Death Sentence'}
+local dif2 = {'', 'Å', 'Å', 'Å', 'Ä', 'Ç', 'É'}
 
 hook.Add( "InitPostEntity", "pd2_load_brief", function()
 	if game.GetMap() == "pd2_jewelry_store_mission" then
@@ -15,6 +17,7 @@ hook.Add( "InitPostEntity", "pd2_load_brief", function()
 end)
 
 hook.Add("HUDPaint", "IconOfPrisonpd2", function()
+	surface.CreateFont("timer_font",{font="pd2",size=60,weight=800} )
 	if LocalPlayer():GetNWBool("pd2prison") then
 		surface.SetDrawColor( 255, 255, 255, 255 ) -- Set the drawing color
 		surface.SetMaterial( ourMat ) -- Use our cached material
