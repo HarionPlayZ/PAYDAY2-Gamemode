56dc0b426e076ff1f1b350d49e54aa162a18ef6e
Shark-vil <shark_vil@mail.ru>
2021-04-19 22:58:08 +0300

    Move the initialization of cvars to shared. File init order changed.

--- gamemodes/payday/gamemode/init.lua
+++ gamemodes/payday/gamemode/init.lua
@@ -1,26 +1,12 @@
-resource.AddFile("materials/pr1.png")
-CreateConVar("padpd2", 0)
+resource.AddFile('materials/pr1.png')
+CreateConVar('padpd2', 0)
 
-AddCSLuaFile ( "shared.lua" )
-include ('shared.lua')
-
-include ('server/pd2_hud.lua')
-include ('server/pd2_assautphases.lua')
-include ('server/pd2_assault_settings.lua')
-include ('server/bleedout_init.lua')
-include("bleedout/bleedout.lua")
-include ('server/custody_pd2.lua')
-include ('server/armorregen.lua')
-include('server/sh_ablative_armor.lua')
-include('server/functions.lua')
-include('server/pd2_teams.lua')
-include('server/pd2_moneyshop.lua')
-include('server/pd2_xplevel.lua')
-include('shared/pd2_class_triggers.lua')
-
-AddCSLuaFile("bleedout/bleedout.lua")
+--* Global registration
+--? For subsequent call on the client side
+AddCSLuaFile('shared.lua')
+AddCSLuaFile('bleedout/bleedout.lua')
 AddCSLuaFile('client/pd2_timer.lua')
-AddCSLuaFile("client/pd2text.lua")
+AddCSLuaFile('client/pd2text.lua')
 AddCSLuaFile('client/pd2_show_items.lua')
 AddCSLuaFile('client/custody_pd2_icon.lua')
 AddCSLuaFile('client/bleedout_client.lua')
@@ -28,14 +14,29 @@ AddCSLuaFile('client/pd2_assautphases.lua')
 AddCSLuaFile('client/pd2_hud.lua')
 AddCSLuaFile('client/pd2_outline.lua')
 AddCSLuaFile('client/bleedout_client.lua')
-
-
 AddCSLuaFile('shared/pd2_class_triggers.lua')
-AddCSLuaFile ( "cl_init.lua" )
+AddCSLuaFile('cl_init.lua')
 
-function GM:PlayerSetHandsModel (ply, ent)
-local simplemodel = player_manager.TranslateToPlayerModelName( ply:GetModel() )
-local info = player_manager.TranslatePlayerHands (simplemodel)
+--* Server script initialization
+--? shared.lua - is called simultaneously on both the client and the server.
+--? Recommended only for generic elements
+include('shared.lua')
+include('server/pd2_hud.lua')
+include('server/pd2_assautphases.lua')
+include('server/pd2_assault_settings.lua')
+include('bleedout/bleedout.lua')
+include('server/custody_pd2.lua')
+include('server/armorregen.lua')
+include('server/sh_ablative_armor.lua')
+include('server/functions.lua')
+include('server/pd2_teams.lua')
+include('server/pd2_moneyshop.lua')
+include('server/pd2_xplevel.lua')
+include('shared/pd2_class_triggers.lua')
+
+function GM:PlayerSetHandsModel(ply, ent)
+	local simplemodel = player_manager.TranslateToPlayerModelName(ply:GetModel())
+	local info = player_manager.TranslatePlayerHands(simplemodel)
 	if info then
 		ent:SetModel(info.model)
 		ent:SetSkin(info.skin)
--- gamemodes/payday/gamemode/shared.lua
+++ gamemodes/payday/gamemode/shared.lua
@@ -1,4 +1,10 @@
-GM.Name 	= "PAYDAY 2"
-GM.Author 	= "HarionPlayZ & Soloveu Empty"
+-- -------------------------------------------------
+-- Other thanks for Delta, [FG] Shark_vil, Jaff.
+-- -------------------------------------------------
 
---other thanks for Delta, [FG] Shark_vil, Jaff.
+GM.Name = 'PAYDAY 2'
+GM.Author = 'HarionPlayZ & Soloveu Empty'
+
+--* Is called simultaneously on both the client and the server.
+--? Recommended only for generic elements
+include('shared/bleedout_init.lua')
