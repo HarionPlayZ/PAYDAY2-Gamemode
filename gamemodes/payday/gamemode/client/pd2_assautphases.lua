45e43d150c32b039bba74602e53b8beae55e2934
HarionPlayZ <debugkeklov@yandex.ru>
2021-04-24 15:35:03 +0300

    Added 3 musics(Break the Rules, Le Castle Vania, Left In The Cold)

--- gamemodes/payday/gamemode/client/pd2_assautphases.lua
+++ gamemodes/payday/gamemode/client/pd2_assautphases.lua
@@ -297,6 +297,14 @@ local DefaultMusic = {
 		Bridge = {""},
 		Assault = {"ass"},
 	},
+	["break_the_rules"] = {
+		Name = "Break The Rules",
+		Stealth = {"st"},
+		Control = {"cont"},
+		Anticipation = {"ant"},
+		Bridge = {""},
+		Assault = {"ass"},
+	},
 }
 local CustomMusic = util.JSONToTable(file.Read("pd2_assaultphases_custommusic.txt","DATA") or "") or {}
 
